461d8d62a1b1c71f6b892d494d6c5fb36b2e84a7
tmj22 <tom_mj91@hotmail.com>
2017-10-06 21:27:49 +0200

    Terminado controlador de usuario
    Registro, login, actualización de perfil y carga de imagen

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,10 @@
 'use strict'
+var fs = require('fs');
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
+var jwt = require('../services/jwt');
+
 
 function pruebas(req, res) {
   res.status(200).send({
@@ -54,7 +58,131 @@ function saveUser(req, res) {
   }
 }
 
+function loginUser(req, res) {
+  var params = req.body;
+  var email = params.email;
+  var password = params.password;
+
+  User.findOne({
+    email: email.toLowerCase()
+  }, (err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error en la peticion'
+      });
+    } else {
+      if (!user) {
+        res.status(404).send({
+          message: 'El usuario no existe'
+        });
+      } else {
+
+        //comprobar la contraseña
+        bcrypt.compare(password, user.password, function(err, check) {
+          if (check) {
+            //devolver los datos del usuario loggeado
+            if (params.gethash) {
+              //devolver un token de jwt
+              res.status(200).send({
+                token: jwt.createToken(user)
+              });
+            } else {
+              res.status(200).send({
+                user
+              });
+            }
+          } else {
+            res.status(404).send({
+              message: 'El usuario no ha podido loggearse'
+            });
+          }
+        });
+      }
+    }
+  });
+}
+
+function updateUser(req, res) {
+  var userId = req.params.id;
+  var update = req.body;
+
+  User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error al actualizar el perfil'
+      });
+    } else {
+      if (!userUpdated) {
+        res.status(404).send({
+          message: 'No se ha podido actualizar el perfil'
+        });
+      } else {
+        res.status(200).send({
+          user: userUpdated
+        });
+      }
+    }
+  });
+}
+
+function uploadImage(req, res) {
+  var userId = req.params.id;
+  var file_name = "No subido... ";
+  if (req.files) {
+    var file_path = req.files.image.path;
+    var file_split = file_path.split('\\');
+    var file_name = file_split[2];
+
+    var ext_split = file_name.split('\.');
+    var file_ext = ext_split[1];
+
+    if (file_ext == "png" || file_ext == "jpg" || file_ext == "gif"  ) {
+      User.findByIdAndUpdate(userId, {
+        image: file_name
+      }, (err, userUpdated) => {
+        if (!userUpdated) {
+          res.status(404).send({
+            message: 'No se ha podido actualizar el perfil'
+          });
+        } else {
+          res.status(200).send({
+            user: userUpdated
+          });
+        }
+      });
+
+    } else {
+      res.status(200).send({
+        message: 'Extensión del archivo no válida'
+      });
+    }
+
+  } else {
+    res.status(200).send({
+      message: 'No se ha subido la imagen'
+    });
+  }
+}
+
+function getImageFile(req, res){
+  var imageFile = req.params.imageFile;
+  var path_file = './uploads/users/'+imageFile;
+
+  fs.exists(path_file, function(exists){
+    if (exists) {
+      res.sendFile(path.resolve(path_file));
+    }else {
+      res.status(200).send({message: 'No existe la imagen'});
+    }
+  });
+}
+
+
 module.exports = {
   pruebas,
-  saveUser
+  saveUser,
+  loginUser,
+  updateUser,
+  uploadImage,
+  getImageFile
 };
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,18 @@
 //cargamos express y el controlador de usuario
 var express = require('express');
 var UserController = require('../controllers/user');
+var md_auth = require('../middlewares/authenticate');
+
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './uploads/users'});
 
 var api = express.Router();
 //ruta y método a cargar
-api.get('/probando-controlador', UserController.pruebas);
+api.get('/probando-controlador',md_auth.ensureAuth , UserController.pruebas);
 api.post('/register', UserController.saveUser);
+api.post('/login', UserController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+api.post('/upload-image-user/:id',[md_auth.ensureAuth, md_upload],  UserController.uploadImage);
+api.get('/get-image-user/:imageFile',  UserController.getImageFile);
 
 module.exports = api;
